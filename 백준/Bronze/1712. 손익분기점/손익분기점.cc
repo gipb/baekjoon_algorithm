e369ee0a455aed019fc7d18d4ab3f14236ab5b34
gipb <52230457+gipb@users.noreply.github.com>
2022-03-16 18:25:04 +0900

    [Bronze IV] Title: 손익분기점, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cc
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int A,B,C;
+    scanf("%d %d %d",&A,&B,&C);
+    if (B>=C)
+        printf("-1");
+    else
+        printf("%d", A/(C-B)+1);
+    return 0;
+}
