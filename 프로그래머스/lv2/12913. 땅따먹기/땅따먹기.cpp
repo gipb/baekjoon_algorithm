65b6bf3be18cffdbd445d583b7324a9eed388c6f
gipb <52230457+gipb@users.noreply.github.com>
2022-04-02 11:10:15 +0900

    [level 2] Title: 땅따먹기, Time: 6.10 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.cpp
@@ -2,34 +2,10 @@
 #include <vector>
 using namespace std;
 
-int Max_(int a, int b, int c){
-    if(a>b){
-        if(a>c) return a;
-        else return c;
-    }
-    else{
-        if(b>c) return b;
-        else return c;
-    }
-}
-
 int solution(vector<vector<int>> land)
 {
     int answer = 0;
-    int N = land.size();
-    for(int i=1; i<N;i++){
-        for(int j=0; j<4; j++){
-            land[i][j] += 
-                Max_(land[i-1][(j+1)%4], 
-                     land[i-1][(j+2)%4], 
-                     land[i-1][(j+3)%4]);
-        }
-    }
-    
-    answer = land[N-1][0];
-    for(int i=1; i<4; i++){
-        if(answer < land[N-1][i]) answer = land[N-1][i];
-    }
+
     
     return answer;
 }
