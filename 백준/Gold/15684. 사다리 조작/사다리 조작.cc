e42414fe411c18f2f4bf44822e946f0fd0a99265
gipb <52230457+gipb@users.noreply.github.com>
2022-04-19 02:01:18 +0900

    Update 사다리 조작.cc
    add comment

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -6,6 +6,7 @@ vector<vector<int>> MAP;
 int N, M, H;
 int ans = 4;
 
+// 사다리 체크
 int check() {
 	for (int c = 1; c <= N; c++) {
 		int cur_c = c;
@@ -17,8 +18,10 @@ int check() {
 	return 0;
 }
 
+
 void dfs(int level) {
 	if (level >= ans) return;
+	// 성공할 경우 ans에 추가된 사다리 개수 등록
 	if (!check()) {
 		if (ans > level) ans = level;
 		return;
@@ -26,8 +29,13 @@ void dfs(int level) {
 
 	for (int r = 1; r <= H; r++) {
 		for (int c = 1; c < N; c++) {
+			// dfs 실행 도중 성공한 분기가 있다면 return
 			if (level >= ans) return;
+			
+			// 사다리를 놓을 수 없는 경우 배제
 			if (MAP[r][c] || MAP[r][c+1]) continue;
+			
+			// 새로운 사다리 놓기
 			MAP[r][c] = c + 1;
 			MAP[r][c + 1] = c;
 			dfs(level + 1);
@@ -42,6 +50,8 @@ int main()
 	cin >> N >> M >> H;
 
 	MAP = vector<vector<int>>(H+1, vector<int>(N+1, 0));
+	
+	// 최소한 각 가로줄은 세로줄 사이에서 짝수 개로 연결되어야 함.
 	vector<int> dat(N + 1, 0);
 
 	int odd_cnt = 0;
@@ -58,10 +68,12 @@ int main()
 		MAP[a][b+1] = b;
 	}
 
+	// 추가 연결이 필요한 사다리 갯수 확인
 	for (int i = 1; i <= N; i++) {
 		if (dat[i] % 2) odd_cnt++;
 	}
-
+	
+	// 추가되어야 하는 사다리의 최소 갯수가 4개 이상인 것들은 배제
 	if(odd_cnt<=3)
 		dfs(0);
 
