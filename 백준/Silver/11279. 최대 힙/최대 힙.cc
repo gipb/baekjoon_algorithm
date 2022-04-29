3b2edc822a2a9b2199b6b17a876fc649cf3ffa1b
gipb <52230457+gipb@users.noreply.github.com>
2022-04-29 18:50:30 +0900

    [Silver II] Title: 최대 힙, Time: 64 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio;
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	priority_queue<int> pq;
+
+	int N;
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+		if (num) pq.push(num);
+		else if (pq.empty()) cout << 0 << '\n';
+		else {
+			cout << pq.top() << '\n';
+			pq.pop();
+		}
+	}
+
+	return 0;
+}
