321b2b61d50431c6543677b0dd2ef5c0c697a233
gipb <52230457+gipb@users.noreply.github.com>
2022-07-07 13:08:06 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,65 +1,66 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 #include <queue>
 using namespace std;
 
 struct Info {
-	int dist;
 	int r, c;
+	int cnt;
 };
 
-int main() {
-	
-	int N, M;
-	cin >> N >> M;
+int n, m;
 
-	vector<vector<int>> MAP(N, vector<int>(M, 0));
-	vector<vector<int>> used(N, vector<int>(M, 0));
-	for (int i = 0; i < N; i++) {
-		string txt;
-		cin >> txt;
-		for (int j = 0; j < M; j++) {
-			MAP[i][j] = txt[j] - '0';
-		}
+vector<string> MAP;
+vector<vector<int>> used;
+
+bool inRange(int r, int c) {
+	return r >= 0 && r < n&& c >= 0 && c < m;
+}
+
+bool isValid(int r, int c) {
+	return MAP[r][c] == '1';
+}
+
+int main(){
+
+	cin >> n >> m;
+
+	used = vector<vector<int>>(n, vector<int>(m, 0));
+
+	for (int i = 0; i < n; i++) {
+		string s;
+		cin >> s;
+		MAP.push_back(s);
 	}
-
-	// 방향 배열
+	
+	// 방향배열
 	int dr[] = { -1,1,0,0 };
 	int dc[] = { 0,0,-1,1 };
 
-	int ans = 0;
-
-	// bfs 활용
 	queue<Info> q;
-
-	q.push({ 1,0,0 });
+	q.push({ 0,0,1 });
 	used[0][0] = 1;
 
 	while (!q.empty()) {
 		Info now = q.front();
 		q.pop();
-		
-		int r = now.r;
-		int c = now.c;
-		int dist = now.dist;
 
-		if (r == N - 1 && c == M - 1) {
-			ans = dist;
+
+		if (now.r == n - 1 && now.c == m - 1) {
+			cout << now.cnt;
 			break;
 		}
 
 		for (int i = 0; i < 4; i++) {
-			int nr = r + dr[i];
-			int nc = c + dc[i];
-			if (nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-			if (!MAP[nr][nc] || used[nr][nc]) continue;
-			q.push({ dist + 1, nr, nc });
+			int nr = now.r + dr[i];
+			int nc = now.c + dc[i];
+			if (!inRange(nr, nc) || used[nr][nc]) continue;
+			if (!isValid(nr, nc)) continue;
+			q.push({ nr,nc , now.cnt+1});
 			used[nr][nc] = 1;
 		}
 	}
-
-	cout << ans;
 
 	return 0;
 }
