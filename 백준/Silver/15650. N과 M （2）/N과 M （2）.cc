cca0373bbc76102926a24874a339a4e110b6f450
gipb <52230457+gipb@users.noreply.github.com>
2022-09-25 02:22:09 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n, m;
+
+vector<int> path;
+
+void dfs(int level) {
+	if (level == m) {
+		for (int i = 0; i < path.size(); i++) {
+			cout << path[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+
+	int start = 1;
+	if (level) start = path[level - 1] + 1;
+
+	for (int i = start; i <= n; i++) {
+		path.push_back(i);
+		dfs(level + 1);
+		path.pop_back();
+	}
+}
+
+int main() {
+	cin >> n >> m;
+
+	dfs(0);
+
+	return 0;
+}
